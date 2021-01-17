cad0064e33899a77ff637754eda358957a3476cb
Wesley <wesley.silva@muralis.com.br>
2021-01-17 11:14:02 -0300

    add first producer and consumers

---
+++ ecommerce/src/main/java/br/com/kafka/ecommerce/EmailService.java
@@ -0,0 +1,53 @@
+package br.com.kafka.ecommerce;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class EmailService {
+
+	public static void main(String...strings) throws InterruptedException {
+		
+		var consumer = new KafkaConsumer<String, String>(properties());
+		
+		consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
+		
+		while(true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+			
+			if(!records.isEmpty()) {
+				print("Found " + records.count() + " register(s).");
+				for(var record : records) {
+					print("-----------------------------------------",
+							"Sending e-mail, checking for fraud.",
+							record.key(),
+							record.value(),
+							String.valueOf(record.partition()),
+							String.valueOf(record.offset()));
+					Thread.sleep(1000);
+				}
+				print("E-mail sent.");
+			}
+			
+		}
+	}
+
+	private static Properties properties() {
+		var properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+		return properties;
+	}
+	
+	private static void print(String...strings) {
+		for(String string : strings)
+			System.err.println(string);
+	}
+}
