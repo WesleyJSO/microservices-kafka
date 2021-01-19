38e74ddf7b93b4c62dabc6f6954d0b80f07fd562
Wesley <wesley.silva@muralis.com.br>
2021-01-18 22:15:56 -0300

    add Gson to serialize and deserialize data

---
+++ ecommerce/src/main/java/br/com/kafka/ecommerce/util/GsonDeserializer.java
@@ -0,0 +1,36 @@
+package br.com.kafka.ecommerce.util;
+
+import java.util.Map;
+
+import org.apache.kafka.common.serialization.Deserializer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonDeserializer<T> implements Deserializer<T> {
+
+	public static final String TYPE_CONFIG = "type_config";
+	
+	private final Gson gson = new GsonBuilder().create();
+
+	private Class<T> clazz;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void configure(Map<String, ?> configs, boolean isKey) {
+		String typeName = String.valueOf(configs.get(TYPE_CONFIG));
+		try {
+			clazz = (Class<T>) Class.forName(typeName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Type for deserialization doesn't exist.");
+	
+		}
+	}
+	
+	@Override
+	public T deserialize(String topic, byte[] data) {
+		return gson.fromJson(new String(data), clazz);
+	}
+	
+	
+}
